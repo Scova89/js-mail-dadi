4077757db7124afe460480617057e0876bd2683a
Francesco <scovascova@gmail.com>
2021-11-10 16:54:58 +0100

    gioco del dado

--- js/main.js
+++ js/main.js
@@ -13,13 +13,37 @@
 // definire un array della lista di mail
 const mail = ["a@gmail.com", "b@gmail.com", "c@gmail.com", "d@gmail.com", "e@gmail.com", "f@gmail.com"];
 
+
 // definire la condizione in cui se la mail inserita corrisponde a quelle dell'array è corretta, altrimenti errata
 let mailInserita = prompt('Inserisci la tua mail');
 console.log(mailInserita);
 
+let verify = false;
+
 for (let i = 0; i < mail.length; i++) {
-    if (mailInserita = mail[i]) {
-        document.getElementById('risposta').innerHTML = 'La mail è corretta'; 
+    if (mail[i] == mailInserita) {
+        verify = true;
     }
-    
 }
+
+
+// definire le variabile del giocatore e del pc con numeri random generati
+let giocatore = Math.floor(Math.random() * 6) + 1;
+console.log('Risultato del dado' + giocatore)
+let pc = Math.floor(Math.random() * 6) + 1;
+console.log('Risultato del dado' + pc)
+
+
+
+if (verify){
+    document.getElementById('risposta').innerHTML = 'La mail è corretta';
+    // verificare il vincitore al gioco dei dadi tramite le condizioni
+    if (giocatore > pc){
+        document.getElementById('risposta').innerHTML = 'Il giocatore ha vinto';
+    }
+     
+}else{
+    document.getElementById('risposta').innerHTML = 'La mail non è corretta';
+}
+
+
